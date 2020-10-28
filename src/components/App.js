311f34a4d57e7f6b55df8e225ac341af91f1141c
clementbrunet42@gmail.com <clementbrunet42@gmail.com>
2020-10-28 14:37:51 +0100

    changes files

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../img/logo.svg';
+import '../styles/App.css';
 
 function App() {
   return (
-    <div class="App">
-      <header class="App-header">
-        <img src={logo} class="App-logo" alt="logo" />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          class="App-link"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// import {Router, Router, browserHistory, IndexRoute} from "react-router";
+
+import './styles/index.css';
+import App from './components/App';
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Router history={browserHistory}>
+//         <Route path={"/"} component={Root}>
+//           <IndexRoute component={Home} />
+//           <Route path={"user"} component={User} />
+//           <Route path={"home"} component={Home} />
+//         </Route>
+//       </Router>
+//     );
+//   }
+// }
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
