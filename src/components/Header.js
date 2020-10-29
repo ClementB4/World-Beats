6f6690bd072a1b272c1dd492dc7cc86b68a86b37
clementbrunet42@gmail.com <clementbrunet42@gmail.com>
2020-10-29 11:49:37 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap"
 
+import logo from '../img/world_beats_logo.png'
+
+ 
 export const Header = (props) => {
     return (
-        <nav className="navbar navbar-default">
-            <div className="container">
-                <div className="navbar-header">
-                    <ul className="nav navbar-nav">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/feuille_de_perso">Feuille de Perso</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="/">
+                <img 
+                    src={logo}
+                    width="180"
+                    height="50"
+                    alt="World Beats Logo"
+                    className="d-inline-block align-top" />
+            </Navbar.Brand>
+            <Nav variant="tabs">
+                <Nav.Item>
+                    <Nav.Link href="/feuille_de_perso">Personnage</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/marchand" eventKey="link-1">Marchand</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/journal_de_bord" eventKey="link-2">Journal de bord</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </Navbar>
     );
 };
